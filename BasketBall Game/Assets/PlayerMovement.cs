2caad0a9ba4571d4e2ec928e31940646a61594f5
ykurebayashi <yngwie.rs2@gmail.com>
2021-07-05 23:50:27 -0300

    Main Mechanics
    - Movement
    - Throw animation
    - Dribble animation

---
+++ BasketBall Game/Assets/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public     Rigidbody   rb;
+
+    public  float   moveSpeed;
+
+    private float   xInput;
+    private float   zInput;
+
+    public  float   jumpForce;
+
+    public Animator    anim;
+
+    public  GameObject ball;
+
+    public  GameObject  locationBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Shoot();
+    }
+
+    private void    FixedUpdate(){
+        Move();
+    }
+
+    private void    Move(){
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
+        rb.AddForce(new Vector3 (xInput, 0, zInput) * moveSpeed);
+    }
+
+    private void    Jump(){
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+        }
+    }
+
+    private void    Shoot(){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            anim.SetBool("Shooting", true);
+            StartCoroutine(CoroutineShoot());
+            StartCoroutine(CoroutineShoot2());
+        }
+    }
+
+    IEnumerator CoroutineShoot(){
+        yield return    new WaitForSeconds(2);
+        anim.SetBool("Shooting", false);
+    }
+
+    IEnumerator CoroutineShoot2(){
+        yield return    new WaitForSeconds(1.5f);
+        Instantiate(ball, locationBall.transform.position, Quaternion.identity);
+    }
+}
---
+++ BasketBall Game/Assets/ballAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballAction : MonoBehaviour
+{
+    private Rigidbody   rbBall;
+    // Start is called before the first frame update
+
+    void    Awake(){
+        rbBall = GetComponent<Rigidbody>();
+
+    }
+    void Start()
+    {
+        rbBall.AddForce(new Vector3(0, 300, 300));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
